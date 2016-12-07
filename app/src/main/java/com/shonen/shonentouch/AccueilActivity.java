3c9a73d3914e26b68303906156f22da5d698c56d
nathanFrancois <nat36200@gmail.com>
2016-12-07 21:07:37 +0100

    Initialisation du projet ShonenTouch

---
+++ app/src/main/java/com/shonen/shonentouch/AccueilActivity.java
@@ -0,0 +1,13 @@
+package com.shonen.shonentouch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccueilActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+    }
+}
