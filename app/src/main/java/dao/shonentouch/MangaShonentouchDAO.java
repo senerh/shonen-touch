def97cd9e127491a4491dba11519b80b13701cf8
Hakan Sener <h-akan@hotmail.fr>
2016-12-23 14:28:02 +0100

    add path constant

--- app/src/main/java/dao/shonentouch/MangaShonentouchDAO.java
+++ app/src/main/java/dao/shonentouch/MangaShonentouchDAO.java
@@ -7,13 +7,15 @@ import dto.Manga;
 
 public class MangaShonentouchDAO extends AbstractShonentouchDAO<List<Manga>> {
 
+    private static final String PATH = "/mangas";
+
     public MangaShonentouchDAO(InterfaceTaskActivity<List<Manga>> interfaceTaskActivity) {
         super(interfaceTaskActivity);
     }
 
     @Override
     protected List<Manga> doInBackground(Void... params) {
-        List<Manga> mangaList = UtilsShonentouchDAO.getList("/mangas", Manga.class);
+        List<Manga> mangaList = UtilsShonentouchDAO.getList(PATH, Manga.class);
         return mangaList;
     }
 
