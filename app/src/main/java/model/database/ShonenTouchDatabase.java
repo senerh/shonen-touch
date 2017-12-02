80fd25cf949c34f46698329d4e397629746f0829
Thibaut SORIANO <st405262@etu.unice.fr>
2017-12-02 16:41:34 +0100

    database creation sql queries

--- app/src/main/java/model/database/ShonenTouchDatabase.java
+++ app/src/main/java/model/database/ShonenTouchDatabase.java
@@ -19,24 +19,26 @@ public class ShonenTouchDatabase extends SQLiteOpenHelper {
          */
         String CREATE_MANGA =
                 "CREATE TABLE " + ShonenTouchContract.Manga.TABLE_NAME + " (" +
-                        ShonenTouchContract.MangaColumns._ID + " INTEGER PRIMARY KEY, " +
-                        ShonenTouchContract.MangaColumns.NAME + " TEXT NOT NULL UNIQUE, " +
-                        ShonenTouchContract.MangaColumns.SLUG + " TEXT);";
+                        ShonenTouchContract.MangaColumns._ID + " BIGINT PRIMARY KEY, " +
+                        ShonenTouchContract.MangaColumns.NAME + " TEXT, " +
+                        ShonenTouchContract.MangaColumns.SLUG + " TEXT UNIQUE);";
 
         String CREATE_SCAN =
                 "CREATE TABLE " + ShonenTouchContract.Scan.TABLE_NAME + " (" +
-                        ShonenTouchContract.ScanColumns._ID + " INTEGER PRIMARY KEY, " +
-                        ShonenTouchContract.ScanColumns.NAME + " TEXT NOT NULL UNIQUE, " +
-                        ShonenTouchContract.ScanColumns.MANGA_ID + " INTEGER," +
+                        ShonenTouchContract.ScanColumns._ID + " BIGINT PRIMARY KEY, " +
+                        ShonenTouchContract.ScanColumns.NAME + " TEXT, " +
+                        ShonenTouchContract.ScanColumns.DOWNLOAD_TIMESTAMP + " BIGINT, " +
+                        ShonenTouchContract.ScanColumns.LAST_READ_PAGE + " INTEGER, " +
+                        ShonenTouchContract.ScanColumns.STATUS + " TEXT NOT NULL, " +
+                        ShonenTouchContract.ScanColumns.MANGA_ID + " BIGINT," +
                         "FOREIGN KEY (" + ShonenTouchContract.ScanColumns.MANGA_ID + ")" +
                         "REFERENCES " + ShonenTouchContract.Manga.TABLE_NAME + " (" + ShonenTouchContract.MangaColumns._ID + ") ON DELETE CASCADE);";
 
         String CREATE_PAGE =
                 "CREATE TABLE " + ShonenTouchContract.Page.TABLE_NAME + " (" +
-                        ShonenTouchContract.PageColumns._ID + " INTEGER PRIMARY KEY, " +
-                        ShonenTouchContract.PageColumns.PATH + " TEXT NOT NULL UNIQUE, " +
-//                        ShonenTouchContract.PageColumns.MANGA_ID + " INTEGER," +
-                        ShonenTouchContract.PageColumns.SCAN_ID + " INTEGER," +
+                        ShonenTouchContract.PageColumns._ID + " BIGINT PRIMARY KEY, " +
+                        ShonenTouchContract.PageColumns.PATH + " TEXT UNIQUE, " +
+                        ShonenTouchContract.PageColumns.SCAN_ID + " BIGINT," +
                         "FOREIGN KEY (" + ShonenTouchContract.PageColumns.SCAN_ID + ")" +
                         "REFERENCES " + ShonenTouchContract.Scan.TABLE_NAME + " (" + ShonenTouchContract.ScanColumns._ID + ") ON DELETE CASCADE);";
     }
